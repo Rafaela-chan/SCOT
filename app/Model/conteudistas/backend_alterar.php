683f58525c41217ec8f712683bc0bfb0846d01b7
LeandroLDomingos <leandrolucasdomingos15@hotmail.com.br>
2023-04-17 15:46:14 -0300

    NEW

--- app/Model/conteudistas/backend_alterar.php
+++ app/Model/conteudistas/backend_alterar.php
@@ -2,6 +2,7 @@
 $cpf_conteudista = $_POST['cpf_conteudista'];
 $nome_conteudista = $_POST['nome_conteudista'];
 $id_conteudista = $_POST['id_conteudista'];
+$id_curso = $_POST['curso_conteudista'];
 
 $servername = "localhost";
 $username = "root";
@@ -14,6 +15,8 @@ try {
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $sql = "UPDATE `conteudista` SET `nome_completo` = '$nome_conteudista', `cpf` = '$cpf_conteudista' WHERE `conteudista`.`id_conteudista` = '$id_conteudista';";
   $conn->exec($sql);
+  $sql1 = "UPDATE `lista_conteudista` SET `id_curso` = '$id_curso' WHERE `lista_conteudista`.`id_conteudista` = '$id_conteudista';";
+  $conn->exec($sql1);
   echo "<script> alert('Conteudista atualizado.'); window.location.href='../../../conteudistas/listar';</script>";
 } catch(PDOException $e) {
   //echo $sql . "<br>" . $e->getMessage(); 
--- app/js/script.js
+++ app/js/script.js
@@ -50,8 +50,10 @@ function dropTutor() {
 }
 
 function listaConteudistas(id_conteudista) {
-   var id_om = 0;
-   console.log(id_om);
+    $('select[name=om_conteudista]').empty();
+    $('select[name=om_conteudista]').append('<option id = "id_om" class="id_om" value="" name="nome_om"></option>');
+    $('select[name=curso_conteudista]').empty();
+    $('select[name=curso_conteudista]').append('<option id = "id_curso" class="id_curso" value="" name="nome_curso"></option>');
     $.ajax({
         type: 'GET',
         url: '../../../../SGCOTE/app/Model/webservice.php',
@@ -62,32 +64,32 @@ function listaConteudistas(id_conteudista) {
         dataType: 'json',
         success: function (data) {
             console.log(data);
-            var a = document.getElementsByClassName("id_om");
-            var b = document.getElementsByClassName("curso_conteudista");
-            var x = document.getElementsByClassName("nome_conteudista");
-            var y = document.getElementsByClassName("cpf_conteudista");
-            var z = document.getElementsByClassName("id_conteudista");
+            var nome_om = document.getElementsByClassName("id_om");
+            var curso_conteudista = document.getElementsByClassName("curso_conteudista");
+            var nome_conteudista = document.getElementsByClassName("nome_conteudista");
+            var cpf_conteudista = document.getElementsByClassName("cpf_conteudista");
+            var id_conteudista = document.getElementsByClassName("id_conteudista");
             var i;
-            for (i = 0; i < x.length; i++) {
-            x[i].value = data.nome_completo[0];
+            for (i = 0; i < nome_conteudista.length; i++) {
+                nome_conteudista[i].value = data.nome_completo[0];
             }
-            for (i = 0; i < y.length; i++) {
-                y[i].value = data.cpf[0];
+            for (i = 0; i < cpf_conteudista.length; i++) {
+                cpf_conteudista[i].value = data.cpf[0];
             }
-            for (i = 0; i < z.length; i++) {
-                z[i].value = data.id_conteudista[0];
+            for (i = 0; i < id_conteudista.length; i++) {
+                id_conteudista[i].value = data.id_conteudista[0];
             }
-            for (i = 0; i < a.length; i++) {
-                a[i].value = data.id_om[0];
+            for (i = 0; i < nome_om.length; i++) {
+                nome_om[i].value = data.id_om[0];
                 $('option[name=nome_om]').empty();
                 $('option[name=nome_om]').append(data.nome_om[0]);
             }
-            for (i = 0; i < b.length; i++) {
-                b[i].value = data.nome_curso[0];
+            for (i = 0; i < curso_conteudista.length; i++) {
+                curso_conteudista[i].value = data.id_curso[0];
                 $('option[name=nome_curso]').empty();
                 $('option[name=nome_curso]').append(data.nome_curso[0]);
+                console.log(curso_conteudista[i]);
             }
-        
         }
     });
     $.ajax({
@@ -98,8 +100,30 @@ function listaConteudistas(id_conteudista) {
         },
         dataType: 'json',
         success: function (data) {
+            
+            var conteudo = document.getElementById("id_om").value;
             for (i = 0; i < data.qtd; i++) {
-                $('select[name=om_conteudista]').append('<option value="' + data.id_om[i] + '">' + data.nome_om[i] + '</option>');
+                if (data.id_om[i] != conteudo) {
+                    $('select[name=om_conteudista]').append('<option value="' + data.id_om[i] + '">' + data.nome_om[i] + '</option>');
+                }
+            }
+        }
+    });
+
+    $.ajax({
+        type: 'GET',
+        url: '../../../../SGCOTE/app/Model/webservice.php',
+        data: {
+            acao: 'dropCursos' //Envia esse dado como GET para o webservice 
+        },
+        dataType: 'json',
+        success: function (data) {
+            console.log(data);
+            var conteudo = document.getElementById("id_curso").value;
+            for (i = 0; i < data.qtd; i++) {
+                if (data.id_curso[i] != conteudo) {
+                    $('select[name=curso_conteudista]').append('<option value="' + data.id_curso[i] + '">' + data.nome_curso[i] + '</option>');
+                }
             }
         }
     });
@@ -161,5 +185,5 @@ $(document).ready(function () {
     dropConteudista();
     dropTutor();
     dropAcesso();
-    
+
 });
